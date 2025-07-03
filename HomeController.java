af56d1cea3a272fa79c44eb524b3cc75b34ef9e6
Ahmed Baghni <161574715+Lz079@users.noreply.github.com>
2025-07-03 23:01:19 +0700

    Add files via upload

---
+++ HomeController.java
@@ -0,0 +1,15 @@
+package com.chronicare.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the ChroniCare Application!";
+    }
+}
+
+
